0c5573a9e564ddb1777aaa85547647bcb8d74814
Ayoub-Mentag <ayoubmentag21@gmail.com>
2025-06-15 10:55:55 +0100

    First commit

---
+++ code/byte42.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Byte42 is ERC20 {
+    constructor(address recipient) ERC20("byte42", "BYT") {
+        _mint(recipient, 1000 * 10 ** decimals());
+    }
+}
